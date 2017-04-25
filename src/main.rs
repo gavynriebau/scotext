2ec46277afba8d2f8acf4d95819960d71e4b19cd
Gavyn <gavyn.riebau@gmail.com>
2017-04-25 21:07:18 +0800

    Added verbose argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,53 @@
 
+extern crate clap;
+
 use std::collections::HashMap;
 use std::io::Read;
+use clap::{App, Arg};
 
 fn main() {
+
+    let matches = App::new("scotext")
+        .version("0.1.0")
+        .about("Scores input based on english language character frequency")
+        .author("Gavyn Riebau")
+        .arg(Arg::with_name("verbose")
+             .help("Increases the verbosity of output")
+             .long("verbose")
+             .short("v")
+             .takes_value(false))
+        .get_matches();
+
+    let mut character_scores = get_char_score_map();
+    let mut score = 0.0;
+    let mut stdin = std::io::stdin();
+    let mut buffer = String::new();
+
+    let _ = stdin.read_to_string(&mut buffer);
+    let input = buffer.to_lowercase();
+
+    if matches.is_present("verbose") {
+        println!("Read input: {}", input);
+    }
+
+    for c in input.chars() {
+        if character_scores.contains_key(&c) {
+            let char_score = character_scores.get(&c).unwrap();
+            if matches.is_present("verbose") {
+                println!("Char: {} - {}", c, char_score);
+            }
+            score = score + char_score;
+        }
+    }
+
+    if matches.is_present("verbose") {
+        println!("Character score was: {}", score);
+    } else {
+        println!("{}", score);
+    }
+}
+
+fn get_char_score_map() -> HashMap<char, f32> {
     let mut character_scores = HashMap::new();
 
     character_scores.insert('e', 12.702);	
@@ -32,23 +77,5 @@ fn main() {
     character_scores.insert('q', 0.095);	
     character_scores.insert('z', 0.074);	
 
-    let mut score = 0.0;
-    let mut stdin = std::io::stdin();
-    let mut buffer = String::new();
-
-    let _ = stdin.read_to_string(&mut buffer);
-    let input = buffer.to_lowercase();
-
-    println!("Read: {}", input);
-
-    for c in input.chars() {
-
-        if character_scores.contains_key(&c) {
-            let char_score = character_scores.get(&c).unwrap();
-            println!("Char: {} - {}", c, char_score);
-            score = score + char_score;
-        }
-    }
-
-    println!("Character score was: {}", score);
+    character_scores
 }
